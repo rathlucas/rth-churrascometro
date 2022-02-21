8e52e62e1b18135166a252523634296b0616c818
Lucin <lucin189@gmail.com>
2022-02-21 19:04:31 -0300

    :tada: First commit
    .

---
+++ scripts/script.js
@@ -0,0 +1,6 @@
+// Carne - 400g por pessoa  + 6 horas - 650g
+// Cerveja - 1200ml por pessoa  + 6 horas - 2000ml
+// Refrigerante/água - 1000ml por pessoa  + 6 horas - 1500ml
+
+// Crianças valem por 0,5
+
