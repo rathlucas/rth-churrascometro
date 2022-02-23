aecc28df06a839f3575b6ed6dbd58a61e4d315ef
Lucin <lucin189@gmail.com>
2022-02-23 10:12:34 -0300

    :sparkles: Add imprimirNaTela function
    When the onclick event is triggered, calculoDeGastos returns the data to imprimirNaTela, whom edit the resultados div innerHTML.

--- scripts/script.js
+++ scripts/script.js
@@ -1,48 +1,50 @@
-// Carne - 400g por pessoa  + 6 horas - 650g
-// Cerveja - 1200ml por pessoa  + 6 horas - 2000ml
-// Refrigerante/água - 1000ml por pessoa  + 6 horas - 1500ml
-
-// Crianças valem por 0,5
-
 const inputAdultos = document.getElementById("adultos");
 const inputCriancas = document.getElementById("criancas");
 const inputDuracao = document.getElementById("duracao");
 
 function calculoDeGasto() {
-    const refri = 1000;
-    const cerveja = 1200;
-    const carne = 400;
-
+    
     const duracao = inputDuracao.value;
-    const numeroAdultos = inputAdultos.value;
-    const numeroCriancas = inputCriancas.value;
-
-    if (duracao <= 6) {
-        var calculoCriancas = {
-            quantidade: numeroCriancas,
-            carne: (numeroCriancas * carne) / 2,
-            refri: (numeroCriancas * refri) / 2
-        };
-
-        var calculoAdultos = {
-            quantidade: numeroAdultos,
-            carne: carne * numeroAdultos,
-            cerveja: cerveja * numeroAdultos,
-            refri: refri * numeroAdultos
-        };
-    } else {
-        var calculoAdultos = {
-            quantidade: numeroAdultos,
-            carne: 650 * numeroAdultos,
-            cerveja: 2000 * numeroAdultos,
-            refri: 1500 * numeroAdultos
-        };
+    
+    let carne = 400;
+    let cerveja = 1200;
+    let refri = 1000;
+    
+    if (duracao > 6) {
+        carne = 650;
+        cerveja = 2000;
+        refri = 1500;
     }
 
-    console.log(calculoAdultos);
-    console.log(calculoCriancas);
-
+    const numeroAdultos = inputAdultos.value;
+    const numeroCriancas = inputCriancas.value;
     
+    const calculoTotal = {
+        carne: (numeroAdultos * carne) + (numeroCriancas * (carne / 2)),
+        refri: (numeroAdultos * refri) + (numeroCriancas * (refri / 2)),
+        cerveja: numeroAdultos * cerveja
+    };
+    
+    console.log(calculoTotal);
+
+    return calculoTotal;
 }
 
+const resultadosDiv = document.getElementById("resultados");
 
+function imprimirNaTela() {
+    const calculoImpresso = calculoDeGasto();
+
+    resultadosDiv.innerHTML = `
+        <p>
+            A quantidade necessária de carne será de ${calculoImpresso["carne"]}g.
+        </p>
+        <p>
+            A quantidade necessária de cerveja será de ${calculoImpresso["cerveja"]}ml.
+        </p>
+        <p>
+            A quantidade necessária de refrigerante/água será de ${calculoImpresso["refri"]}ml.
+        </p>
+
+        `
+}
