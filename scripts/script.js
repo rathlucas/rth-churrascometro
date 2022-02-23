e4b8772d35656fb87ce547d53416641df63d81e0
Lucin <lucin189@gmail.com>
2022-02-22 22:15:24 -0300

    :hammer: Add calculoDeGasto function
    .

--- scripts/script.js
+++ scripts/script.js
@@ -4,3 +4,45 @@
 
 // Crianças valem por 0,5
 
+const inputAdultos = document.getElementById("adultos");
+const inputCriancas = document.getElementById("criancas");
+const inputDuracao = document.getElementById("duracao");
+
+function calculoDeGasto() {
+    const refri = 1000;
+    const cerveja = 1200;
+    const carne = 400;
+
+    const duracao = inputDuracao.value;
+    const numeroAdultos = inputAdultos.value;
+    const numeroCriancas = inputCriancas.value;
+
+    if (duracao <= 6) {
+        var calculoCriancas = {
+            quantidade: numeroCriancas,
+            carne: (numeroCriancas * carne) / 2,
+            refri: (numeroCriancas * refri) / 2
+        };
+
+        var calculoAdultos = {
+            quantidade: numeroAdultos,
+            carne: carne * numeroAdultos,
+            cerveja: cerveja * numeroAdultos,
+            refri: refri * numeroAdultos
+        };
+    } else {
+        var calculoAdultos = {
+            quantidade: numeroAdultos,
+            carne: 650 * numeroAdultos,
+            cerveja: 2000 * numeroAdultos,
+            refri: 1500 * numeroAdultos
+        };
+    }
+
+    console.log(calculoAdultos);
+    console.log(calculoCriancas);
+
+    
+}
+
+
